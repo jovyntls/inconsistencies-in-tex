a6581e2308d11b423181ad8df6c80e2beaf14263
Jovyn Tan <jovyn.tls@gmail.com>
2023-08-30 22:55:07 +0800

    update config.py

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
-from utils.logger import Log_level
-
 import os
 
 PROJECT_ROOT = os.path.join(os.path.expanduser("~"), "Desktop", "work", "fyp", "exploration", "diff_test_tex_engines")
+LOGS_FOLDER = os.path.join(PROJECT_ROOT, 'logs')
+
 DOWNLOAD_FOLDER = os.path.join(PROJECT_ROOT, 'bin/arxiv_tars')
 EXTRACTED_FOLDER = os.path.join(PROJECT_ROOT, 'bin/arxiv_tars_extracted')
 COMPILED_FOLDER = os.path.join(PROJECT_ROOT, 'bin/compiled_tex_pdf')
 DIFFS_FOLDER = os.path.join(PROJECT_ROOT, 'bin/diff_pdfs')
 
-LOG_LEVEL = Log_level.INFO
 
 NUM_ATTEMPTS = 50
 TEX_FILE_DOWNLOAD_URL = 'https://arxiv.org/list/cs.IT/2308?skip=0&show=' + str(NUM_ATTEMPTS)
