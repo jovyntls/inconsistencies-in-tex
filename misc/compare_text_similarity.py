f875b0c5f61c0c0bc6f34d3857b26d88a73c3c80
Jovyn Tan <jovyn.tls@gmail.com>
2023-09-22 16:01:10 +0800

    add text transformer

--- misc/compare_text_similarity.py
+++ misc/compare_text_similarity.py
@@ -3,6 +3,7 @@ import fitz  # imports the pymupdf library
 import Levenshtein
 import pandas as pd
 import misc.utils as utils
+import misc.text_transformer as Ttr
 from config import COMPILED_FOLDER
 
 DF_COMPARISON_INDEX = 'comparison'
@@ -10,6 +11,8 @@ DF_COMPARISON_INDEX = 'comparison'
 def init_df():
     cols = [DF_COMPARISON_INDEX, 'xepdf', 'xelua']
     return utils.init_df_with_cols(cols, DF_COMPARISON_INDEX)
+
+# < PDF to string helpers > ---------------------------------------------------
 
 def extract_pages_from_pdf(pdf_path):
     try:
@@ -21,7 +24,6 @@ def extract_pages_from_pdf(pdf_path):
 
 def extract_pages_from_pdfs(COMPILED_FOLDER, arxiv_id):
     compiled_pdfs_dir = os.path.join(COMPILED_FOLDER, arxiv_id)
-    # get text from PDF
     pdf_texts = {}
     for engine in utils.ENGINES:
         pdf_path = os.path.join(compiled_pdfs_dir, f'{arxiv_id}_{engine}latex.pdf')
@@ -30,12 +32,16 @@ def extract_pages_from_pdfs(COMPILED_FOLDER, arxiv_id):
             pdf_texts[engine] = pages
     return pdf_texts
 
-def process_pages_to_string(engine_to_pages, process_f):
+def process_pages_to_string(engine_to_pages, transformer):
     pdf_texts = {}
     for engine, pages in engine_to_pages.items():
         if pages == None: continue
-        pdf_texts[engine] = process_f(pages)
+        pdf_texts[engine] = transformer.process(pages)
     return pdf_texts
+
+# </ PDF to string helpers > --------------------------------------------------
+
+# < text comparison helpers > -------------------------------------------------
 
 def compare_texts_with_method(pdf_texts, compare_f):
     result = {}
@@ -53,17 +59,9 @@ def normalise(f):
         return normalised_score
     return compare
 
-def process_ignore_pagebreak_linebreak(pages_arr):
-    # replace linebreaks with space
-    pages_arr = [page.replace('\n', ' ') for page in pages_arr]
-    # replace pagebreaks with space
-    return ' '.join(pages_arr)
+# </ text comparion helpers > -------------------------------------------------
 
-def process_ignore_hyphenbreak_pagebreak_linebreak(pages_arr):
-    # replace linebreaks with space
-    pages_arr = [page.replace('-\n','\n').replace('\n', ' ') for page in pages_arr]
-    # replace pagebreaks with space
-    return ' '.join(pages_arr)
+# < edit_op helpers > ---------------------------------------------------------
 
 def transform_op(edit_op, s1, s2):
     action, src_index, dest_index = edit_op
@@ -89,6 +87,17 @@ def collate_edit_ops(edit_ops):
         collated[op_index] += 1
     return collated
 
+def analyse_edit_opts_results(edit_ops_results):
+    RESULTS = {}
+    for cmp_engines, df in edit_ops_results.items():
+        grouped = df.groupby('action')['count'].sum().reset_index()
+        RESULTS[cmp_engines] = grouped
+    return RESULTS
+
+# </ edit_op helpers > --------------------------------------------------------
+
+# < runners > -----------------------------------------------------------------
+
 def compute_edit_ops(pdf_texts):
     def compute_edit_ops_for_engine(e1, e2):
         edit_ops = get_edit_ops(pdf_texts[e1], pdf_texts[e2])
@@ -112,10 +121,12 @@ def compute_comparison_metrics(COMPARE_METHODS, pdf_texts):
     df = pd.concat([df, pd.DataFrame.from_records(RESULTS, index=DF_COMPARISON_INDEX)])
     return df
 
-def get_text_from_pdf(arxiv_id, process_f):
+def get_text_from_pdf(arxiv_id, transformer):
     pdf_pages = extract_pages_from_pdfs(COMPILED_FOLDER, arxiv_id)
-    pdf_texts = process_pages_to_string(pdf_pages, process_f)
+    pdf_texts = process_pages_to_string(pdf_pages, transformer)
     return pdf_texts
+
+# </ runners > -----------------------------------------------------------------
 
 COMPARE_METHODS = {
     'levenshtein': Levenshtein.distance,
@@ -125,8 +136,10 @@ COMPARE_METHODS = {
     'hamming_normalised': normalise(Levenshtein.hamming),
 }
 
-def extract_pdf_text_to_save_file(arxiv_id, process_f=process_ignore_hyphenbreak_pagebreak_linebreak):
-    pdf_texts = get_text_from_pdf('2306.' + arxiv_id, process_f)
+DEFAULT_TRANSFORMER = Ttr.transformer_ignore_hyphenbreak_pagebreak_linebreak
+
+def extract_pdf_text_to_save_file(arxiv_id, transformer=DEFAULT_TRANSFORMER):
+    pdf_texts = get_text_from_pdf('2306.' + arxiv_id, transformer)
     # save to file
     for engine, text in pdf_texts.items():
         utils.save_to_file(text, f'{arxiv_id}_{engine}_pdftext.txt')
@@ -137,13 +150,17 @@ def main(user_input):
     # if len(user_input) != 5: return print('invalid input')
     arxiv_id = '2306.' + user_input
 
-    pdf_texts = get_text_from_pdf(arxiv_id, process_f=process_ignore_hyphenbreak_pagebreak_linebreak)
+    # TODO: number of images, placement of images, size of images, number of pages
+    # TODO: can use the edit distance to check for missing content (insertions - deletions)
+    pdf_texts = get_text_from_pdf(arxiv_id, transformer=DEFAULT_TRANSFORMER)
     comparison_results = compute_comparison_metrics(COMPARE_METHODS, pdf_texts)
     edit_ops_results = compute_edit_ops(pdf_texts)
+    analysed_edit_opts_results = analyse_edit_opts_results(edit_ops_results)
 
     for cmp_engines, res in edit_ops_results.items():
         print(f'\n{cmp_engines} [{res.shape[0]} rows]')
-        print(res.head(10))
+        print(res.head(15))
+        print('\n', analysed_edit_opts_results[cmp_engines])
     print(comparison_results)
 
 
---
+++ misc/text_transformer.py
@@ -0,0 +1,33 @@
+
+class TextTransformer:
+    # pre-/post-transformations: applied before/after joining pages
+    def __init__(self, pre_transformations, page_break_delimiter, post_transformations):
+        self.page_break_delimiter = page_break_delimiter
+        self.pre_transformations = pre_transformations
+        self.post_transformations = post_transformations
+
+    @staticmethod
+    def apply_transform(text, transformations):
+        for old, new in transformations:
+            text = text.replace(old, new)
+        return text
+
+    def process(self, pages_arr):
+        processed_pages = [TextTransformer.apply_transform(page, self.pre_transformations) for page in pages_arr]
+        text = self.page_break_delimiter.join(processed_pages)
+        return TextTransformer.apply_transform(text, self.post_transformations)
+
+# common transforms ======================================================================
+HYPHEN_BREAKS_TO_LINE_BREAK = ('-\n', '\n')  # check behaviour when used with line breaks
+# for whitespace
+LINE_BREAKS_TO_SPACES = ('\n', ' ')
+IGNORE_SPACES = (' ', '')
+# ligatures
+COMMON_LIGATURES = [ ('ﬀ', 'ff'), ('ﬁ', 'fi'), ('ﬂ', 'fl'), ('ﬃ', 'ffi') ]
+# accents
+COMMON_ACCENTS = [ ('¨o', 'ö'), ('¨a', 'ä'), ('¨u', 'ü'), ('˚a', 'å'), ('˚A', 'Å') ]
+
+# common transformers ====================================================================
+pretransformers = [ HYPHEN_BREAKS_TO_LINE_BREAK, LINE_BREAKS_TO_SPACES ]
+posttransformers = COMMON_ACCENTS + COMMON_LIGATURES + [ IGNORE_SPACES ]
+transformer_ignore_hyphenbreak_pagebreak_linebreak = TextTransformer(pretransformers, ' ', posttransformers)
