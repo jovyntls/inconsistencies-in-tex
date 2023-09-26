a5f9b222e6c04fc48e00408a224cb4e375faf5f4
Jovyn Tan <jovyn.tls@gmail.com>
2023-09-26 15:24:33 +0800

    move run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,8 +5,4 @@ latexmk -xelatex -jobname="xelatex_compiled" main.tex
 latexmk -lualatex -jobname="lualatex_compiled" main.tex
 diff-pdf --output-diff='xe_pdf_diff.pdf' -smg --dpi=100 --per-page-pixel-tolerance=3000 xelatex_compiled.pdf pdflatex_compiled.pdf
 diff-pdf --output-diff='xe_lua_diff.pdf' -smg --dpi=100 --per-page-pixel-tolerance=3000 xelatex_compiled.pdf lualatex_compiled.pdf
-rm *.aux
-rm *.fls
-rm *.fdb_latexmk
-rm *.dvi
-rm *.xdv
+rm *.aux *.fls *.fdb_latexmk *.dvi *.xdv
