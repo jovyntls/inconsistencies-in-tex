54a9a704b309c0a3f6c55c9ee70aa818e7e47e0c
Jovyn Tan <jovyn.tls@gmail.com>
2024-01-28 21:42:18 +0800

    add img comparison for single file

---
+++ img_comparison/compare_imgs.py
@@ -0,0 +1,82 @@
+import os
+import cv2
+from PIL import Image
+from skimage.metrics import structural_similarity
+import ssim.ssimlib as pyssim
+from config import CONVERTED_IMG_FOLDER
+from utils.logger import COMPARISON_LOGGER as LOGGER
+
+SIM_IMAGE_SIZE = (400, 640)  # TODO get the actual dimensions and move this to config maybe?
+
+# comparison algos
+CMP_ALGORITHMS = {
+    'SSIM': structural_similarity,
+    'CWSSIM': lambda i1,i2: pyssim.SSIM(Image.fromarray(i1)).cw_ssim_value(Image.fromarray(i2)),
+    'ORB': lambda i1,i2: compare_with_score_calculation(i1, i2, 'ORB'),
+    'SIFT': lambda i1,i2: compare_with_score_calculation(i1, i2, 'SIFT')
+}
+CMP_ALGO_THRESHOLDS = {
+    'SSIM': 0.7,
+    'CWSSIM': 0.7,
+    'ORB': 0.7,
+    'SIFT': 0.7
+}
+assert(CMP_ALGORITHMS.keys() == CMP_ALGO_THRESHOLDS.keys())
+
+def compare_with_score_calculation(img1, img2, cmp_method):
+    ALGOS = {
+        'ORB': cv2.ORB_create(),
+        'SIFT': cv2.SIFT_create()
+    }
+    ALGO_RATIOS = { 'ORB': 0.7, 'SIFT': 0.7 }
+    assert(cmp_method in ALGOS and cmp_method in CMP_ALGO_THRESHOLDS)
+    # Read imgs and compare
+    cmp_algo = ALGOS[cmp_method]
+    # img1=cv2.imread(img1,4)
+    # img2=cv2.imread(img2,4)
+    kps1, des1 = cmp_algo.detectAndCompute(img1,None)
+    kps2, des2 = cmp_algo.detectAndCompute(img2,None)
+    # BFMatcher with default params
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1,des2, k=2)
+    similarity_count = sum([ m.distance < ALGO_RATIOS[cmp_method] * n.distance for m,n in matches ])
+    return similarity_count/len(kps1)
+
+def get_similarity_score(img1, img2, algorithm='SSIM'):
+    assert(algorithm in CMP_ALGORITHMS)
+    cmp_method = CMP_ALGORITHMS[algorithm]
+    return cmp_method(img1, img2)
+
+def run_img_comparisons(imgpath1, imgpath2, algorithms=list(CMP_ALGORITHMS.keys())):
+    i1 = cv2.resize(cv2.imread(imgpath1, cv2.IMREAD_GRAYSCALE), SIM_IMAGE_SIZE)
+    i2 = cv2.resize(cv2.imread(imgpath2, cv2.IMREAD_GRAYSCALE), SIM_IMAGE_SIZE)
+    scores = {}
+    for algo in algorithms:
+        score = get_similarity_score(i1, i2, algo)
+        scores[algo] = score  # TODO: use df instead of array
+    return scores
+
+def main(arxiv_id):  # arxiv_id including YYMM
+    img_subdir = os.path.join(CONVERTED_IMG_FOLDER, arxiv_id)
+    if not os.path.exists(img_subdir):
+        LOGGER.debug(f'{img_subdir} not found - skipping')
+        return
+    images_in_dir = os.listdir(img_subdir)
+    # split the images into comparison groups
+    cmp_groups = {}
+    for img_filename_with_ext in images_in_dir:
+        img_filename = os.path.splitext(img_filename_with_ext)[0]
+        _, engine, pg = img_filename.split('_')
+        if pg not in cmp_groups: cmp_groups[pg] = {}
+        cmp_groups[pg][engine] = img_filename_with_ext
+    # perform comparisons
+    for pg, cmp_grp_members in cmp_groups.items():
+        # comparisons within one group
+        CMP_BASELINE = 'xelatex'
+        if len(cmp_grp_members) < 2 or CMP_BASELINE not in cmp_grp_members: continue
+        baseline_img = os.path.join(img_subdir, cmp_grp_members.pop(CMP_BASELINE))
+        for engine, img_filename in cmp_grp_members.items():
+            img_filepath = os.path.join(img_subdir, img_filename)
+            result = run_img_comparisons(baseline_img, img_filepath)
+            print(pg, engine, result)
+
--- run_img_comparison.py
+++ run_img_comparison.py
@@ -7,19 +7,9 @@ import pandas as pd
 
 from utils import logger
 from config import YEAR_AND_MONTH, LOGS_FOLDER, COMPILED_FOLDER
-from img_comparison import convert_pdf_to_img
+from img_comparison import convert_pdf_to_img, compare_imgs
 
 LOGGER = logger.COMPARISON_LOGGER
-
-def run_compare_one(arxiv_id_suffix, should_save, current_time):
-    # don't log to a file if running for only one
-    logger.init_logger(logger.COMPARISON_LOGGER_ID, LOGS_FOLDER, current_time,
-                       console_log_level=logging.DEBUG, has_file_handler=False)
-
-    arxiv_id = f'{YEAR_AND_MONTH}.{arxiv_id_suffix}'
-    convert_pdf_to_img.main(arxiv_id)
-    if should_save:
-        print('save')
 
 def run_compare_all(current_time):
     logger.init_logger(logger.COMPARISON_LOGGER_ID, LOGS_FOLDER, current_time,
@@ -38,18 +28,28 @@ def run_compare_all(current_time):
     # RESULTS_SUMMARY.to_csv(os.path.join(LOGS_FOLDER, f'{current_time}_imgcompare_results.csv'))
 
 
-def run(args):
+def run(run_for_id, do_convert, do_compare):
     current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
     os.makedirs(LOGS_FOLDER, exist_ok=True)
-    if args.id is None: run_compare_all(current_time)
-    else: run_compare_one(args.id, args.save, current_time)
+    if run_for_id is None: run_compare_all(current_time)
+    else: 
+        # don't log to a file if running for only one
+        logger.init_logger(logger.COMPARISON_LOGGER_ID, LOGS_FOLDER, current_time,
+                           console_log_level=logging.DEBUG, has_file_handler=False)
+
+        arxiv_id = f'{YEAR_AND_MONTH}.{run_for_id}'
+        if do_convert: convert_pdf_to_img.main(arxiv_id)
+        if do_compare: compare_imgs.main(arxiv_id)
 
 if __name__ == '__main__':
     # set up CLI args
     parser = argparse.ArgumentParser(description='Run image comparison')
-    parser.add_argument('-save', action='store_true', help="save results to a file")
     parser.add_argument('-id', nargs='?', default=None, help="arxiv_id to run on (runs on all PDFs if not specified)")
+    parser.add_argument('-convert', action='store_true', help="run conversion (of pdf to img) only")
+    parser.add_argument('-compare', action='store_true', help="run image comparison only")
     args = parser.parse_args()
 
-    # leggooooooooooooooooo
-    run(args)
+    do_both = not args.compare and not args.convert
+    do_compare = args.compare or do_both
+    do_convert = args.convert or do_both
+    run(args.id, do_convert, do_compare)
