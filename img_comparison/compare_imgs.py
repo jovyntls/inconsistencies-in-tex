c35497a083fd4a9b676e36b608fed571f1f225eb
Jovyn Tan <jovyn.tls@gmail.com>
2024-02-08 12:20:35 +0800

    allow specifying which cmp algo

--- img_comparison/compare_imgs.py
+++ img_comparison/compare_imgs.py
@@ -13,27 +13,28 @@ CMP_ALGORITHMS = {
     'SSIM': structural_similarity,
     'CWSSIM': lambda i1,i2: pyssim.SSIM(Image.fromarray(i1)).cw_ssim_value(Image.fromarray(i2)),
     'ORB': lambda i1,i2: compare_with_score_calculation(i1, i2, 'ORB'),
-    'SIFT': lambda i1,i2: compare_with_score_calculation(i1, i2, 'SIFT')
+    'SIFT': lambda i1,i2: compare_with_score_calculation(i1, i2, 'SIFT'),
+    'SIFTMAX': lambda i1,i2: compare_with_score_calculation(i1, i2, 'SIFT')
 }
 CMP_ALGO_THRESHOLDS = {
     'SSIM': 0.7,
     'CWSSIM': 0.7,
     'ORB': 0.7,
-    'SIFT': 0.7
+    'SIFT': 0.7,
+    'SIFTMAX': 0.7
 }
 assert(CMP_ALGORITHMS.keys() == CMP_ALGO_THRESHOLDS.keys())
 
 def compare_with_score_calculation(img1, img2, cmp_method):
     ALGOS = {
         'ORB': cv2.ORB_create(),
-        'SIFT': cv2.SIFT_create()
+        'SIFT': cv2.SIFT_create(),
+        'SIFTMAX': cv2.SIFT_create()
     }
-    ALGO_NORM_METHODS = { 'ORB': cv2.NORM_HAMMING, 'SIFT': cv2.NORM_L1 }
+    ALGO_NORM_METHODS = { 'ORB': cv2.NORM_HAMMING, 'SIFT': cv2.NORM_L1, 'SIFTMAX': cv2.NORM_L1 }
     assert(cmp_method in ALGOS and cmp_method in CMP_ALGO_THRESHOLDS and cmp_method in ALGO_NORM_METHODS)
     # Read imgs and compare
     cmp_algo = ALGOS[cmp_method]
-    # img1=cv2.imread(img1,4)
-    # img2=cv2.imread(img2,4)
     kps1, des1 = cmp_algo.detectAndCompute(img1,None)
     kps2, des2 = cmp_algo.detectAndCompute(img2,None)
     num_kps1, num_kps2 = len(kps1), len(kps2)
@@ -47,7 +48,7 @@ def compare_with_score_calculation(img1, img2, cmp_method):
         if len(matches) == 0: return 1
         LOGGER.warn(f'no keypoints found for both images but matches were found')
         return -1
-    elif num_kps1 == 0 or num_kps2 == 0:
+    elif num_kps1 == 0 or num_kps2 == 0 or cmp_method == 'SIFTMAX':
         return similarity_count/max(num_kps1, num_kps2)
     else:
         return similarity_count/min(num_kps1, num_kps2)
@@ -70,7 +71,7 @@ def run_img_comparisons(imgpath1, imgpath2, algorithms=list(CMP_ALGORITHMS.keys(
         scores[algo] = score
     return scores
 
-def main(arxiv_id):  # arxiv_id including YYMM
+def main(arxiv_id, algos):  # arxiv_id including YYMM
     img_subdir = os.path.join(CONVERTED_IMG_FOLDER, arxiv_id)
     if not os.path.exists(img_subdir) or not os.path.isdir(img_subdir):
         LOGGER.debug(f'{img_subdir} not found - skipping')
@@ -93,7 +94,7 @@ def main(arxiv_id):  # arxiv_id including YYMM
         baseline_img = os.path.join(img_subdir, cmp_grp_members.pop(CMP_BASELINE))
         for engine, img_filename in cmp_grp_members.items():
             img_filepath = os.path.join(img_subdir, img_filename)
-            engine_result = run_img_comparisons(baseline_img, img_filepath)
+            engine_result = run_img_comparisons(baseline_img, img_filepath, algos)
             for cmp_algo, value in engine_result.items():
                 result_for_page[f'{CMP_BASELINE[:-5]}{engine[:-5]}_{cmp_algo}'] = value
         RESULT[pg] = result_for_page
--- run_img_comparison.py
+++ run_img_comparison.py
@@ -18,12 +18,12 @@ def convert_for_all():
     for arxiv_id in tqdm(os.listdir(COMPILED_FOLDER)):
         convert_pdf_to_img.main(arxiv_id)
 
-def compare_for_all():
+def compare_for_all(algos):
     dirs = os.listdir(COMPILED_FOLDER)
     rows = []
     LOGGER.info('running image comparisons...')
     for arxiv_id in tqdm(dirs):
-        result = compare_imgs.main(arxiv_id)
+        result = compare_imgs.main(arxiv_id, algos)
         for page, page_results in result.items():
             page_results['identifier'] = f'{arxiv_id}_{page}'
             rows.append(page_results)
@@ -43,7 +43,7 @@ def run(run_for_id, do_convert, do_compare, do_text):
         LOGGER.info(f'{do_convert=}\t{do_compare=}\t{do_text=}')
         if do_convert: convert_for_all()
         if do_compare: 
-            results = compare_for_all()
+            results = compare_for_all(algos=do_compare)
             results.to_csv(os.path.join(LOGS_FOLDER, f'{current_time}_imgcompare.csv'))
         if do_text:
             results = text_cmp.run_text_cmp_for_all()
@@ -53,7 +53,7 @@ def run(run_for_id, do_convert, do_compare, do_text):
                            console_log_level=logging.DEBUG, has_file_handler=False)
         arxiv_id = f'{YEAR_AND_MONTH}.{run_for_id}'
         if do_convert: convert_pdf_to_img.main(arxiv_id)
-        if do_compare: compare_imgs.main(arxiv_id)
+        if do_compare: compare_imgs.main(arxiv_id, algos=do_compare)
         if do_text: 
             result = text_cmp.run_text_cmp(arxiv_id)
             results_as_df = pd.DataFrame.from_records(result, index='identifier')
