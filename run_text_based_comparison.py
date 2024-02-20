066f943480a4c667477027c8148a5537807647fd
Jovyn Tan <jovyn.tls@gmail.com>
2024-02-20 15:45:07 +0800

    add debugging

--- run_text_based_comparison.py
+++ run_text_based_comparison.py
@@ -13,45 +13,50 @@ from text_based_comparison import extract, compare
 
 LOGGER = logger.COMPARISON_LOGGER
 
-def compare_for_one(arxiv_id, should_save):
+def compare_for_one(arxiv_id, should_save, should_save_debug):
     pdf_infos = {}
     for engine in tex_engine_utils.TEX_ENGINES:
         pdf_filepath = os.path.join(COMPILED_FOLDER, arxiv_id, f'{arxiv_id}_{engine}.pdf')
         if not os.path.isfile(pdf_filepath): continue
-        pdf_infos[engine] = extract.get_text_fonts_images(pdf_filepath)
+        pdf_infos[engine], debug_content = extract.get_text_fonts_images(pdf_filepath)
+        # save if needed
+        if should_save_debug:
+            with open(f'text_{engine}.txt', 'w') as file: file.write('\n'.join([ str(x) for x in debug_content ]))
+        elif should_save:
+            with open(f'text_{engine}.txt', 'w') as file: file.write(pdf_infos[engine].text)
 
     # pdf_infos: { pdflatex: ..., xelatex: ... }
     # pdf_texts, pdf_imgs: { pdf: ..., xe: ... }
     pdf_texts = { k[:-5]: compare.text_transformation(content.text) for k, content in pdf_infos.items() }
     pdf_imgs = { k[:-5]: content.images for k, content in pdf_infos.items() }
     pdf_fonts = { k[:-5]: content.fonts for k, content in pdf_infos.items() }
-    for b in pdf_fonts.values(): print(b)
-    # save if needed
-    if should_save:
-        for engine, text in pdf_texts.items():
-            with open(f'text_{engine}.txt', 'w') as file: file.write(text)
+    # for b in pdf_fonts.values(): print(b)
 
     RESULTS = compare.text_comparison(pdf_texts)
     img_cmp_results = compare.run_img_comparison(pdf_imgs)
     font_cmp_results = compare.run_font_comparison(pdf_fonts)
     RESULTS = pd.concat([RESULTS, pd.DataFrame.from_records(img_cmp_results + font_cmp_results, index='comparison')])
-    print(RESULTS)
-    return pdf_infos
+    # print(RESULTS)
+    return RESULTS
 
 def compare_for_all():
     rows = []
     LOGGER.info('running text/format/image comparisons...')
+    tmp = 0
     for arxiv_id in tqdm(os.listdir(COMPILED_FOLDER)):
-        pdf_infos = compare_for_one(arxiv_id, False)
-        # for page, page_results in result.items():
-        #     page_results['identifier'] = f'{arxiv_id}_{page}'
-        #     rows.append(page_results)
-    # RESULTS_SUMMARY = pd.DataFrame.from_records(rows, index='identifier')
-    # LOGGER.debug('full results summary:\n' + RESULTS_SUMMARY.to_string())
-    # return RESULTS_SUMMARY
+        tmp += 1
+        if tmp > 10: break
 
+        LOGGER.debug(f'{arxiv_id=}')
+        result = compare_for_one(arxiv_id, False, False)
+        xelua_result = { f'xelua_{key}': val for key,val in result['xelua'].to_dict().items() }
+        xepdf_result = { f'xepdf_{key}': val for key,val in result['xepdf'].to_dict().items() }
+        result_as_row = { 'arxiv_id': arxiv_id } | xelua_result | xepdf_result
+        rows.append(result_as_row)
+    RESULTS_SUMMARY = pd.DataFrame.from_records(rows, index='arxiv_id')
+    return RESULTS_SUMMARY
 
-def run(run_for_id, should_save):
+def run(run_for_id, should_save, should_save_debug):
     current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
     os.makedirs(LOGS_FOLDER, exist_ok=True)
     if run_for_id is None: 
@@ -60,19 +65,24 @@ def run(run_for_id, should_save):
         warnings.filterwarnings('ignore')
         LOGGER.info(f'logfile timestamp: {current_time}')
         LOGGER.info(f'{should_save=}')
-        compare_for_all()
+
+        RESULTS_SUMMARY = compare_for_all()
+
+        LOGGER.debug('full results summary:\n' + RESULTS_SUMMARY.to_string())
+        RESULTS_SUMMARY.to_csv(os.path.join(LOGS_FOLDER, f'{current_time}_analysis_results.csv'))
     else: 
         logger.init_logger(logger.COMPARISON_LOGGER_ID, LOGS_FOLDER, current_time,
                            console_log_level=logging.DEBUG, has_file_handler=False)
         while len(run_for_id) < 5: run_for_id = '0' + run_for_id
         arxiv_id = f'{YEAR_AND_MONTH}.{run_for_id}'
-        compare_for_one(arxiv_id, should_save)
+        compare_for_one(arxiv_id, should_save, should_save_debug)
 
 if __name__ == '__main__':
     # set up CLI args
     parser = argparse.ArgumentParser(description='Run text-based comparison (text, formatting, images)')
     parser.add_argument('-id', nargs='?', default=None, help="arxiv_id to run on (runs on all PDFs if not specified)")
-    parser.add_argument('-save', action='store_true', help="save the text outputs to tmp{1,2,3}.txt for running diffs")
+    parser.add_argument('-save', action='store_true', help="save the text outputs to text_{pdf,xe,lua}.txt for running diffs")
+    parser.add_argument('-debug', action='store_true', help="save the text output with font information for debugging. overrides -save")
     args = parser.parse_args()
 
-    run(args.id, args.save)
+    run(args.id, args.save, args.debug)
--- text_based_comparison/extract.py
+++ text_based_comparison/extract.py
@@ -1,5 +1,6 @@
 import fitz  # PyMuPDF
 from typing import Dict, List, NamedTuple, Set
+import hashlib
 
 TEXT_EXTRACTION_FLAGS = (fitz.TEXTFLAGS_DICT \
                         | fitz.TEXT_DEHYPHENATE) \
@@ -23,6 +24,7 @@ class PdfContent(NamedTuple):
 
 def get_text_fonts_images(pdf_path: str):
     pdf_document = fitz.Document(pdf_path)
+    debug_content = []
     full_text, text_with_formatting, fonts_used, images = [], {}, set(), []
     for page_num in range(pdf_document.page_count):
         page = pdf_document[page_num]
@@ -31,12 +33,15 @@ def get_text_fonts_images(pdf_path: str):
         blocks = page.get_text("dict", sort=True, flags=TEXT_EXTRACTION_FLAGS)['blocks']
         # block structure: https://pymupdf.readthedocs.io/en/latest/_images/img-textpage.png
         for block in blocks:
-            if block['type'] == 1: continue  # image block
+            if block['type'] == 1:   # image block
+                debug_content.append(ImageInfo(hashlib.md5(block['image']).hexdigest(), (block['width'], block['height'])))
+                continue
             for line in block['lines']:
                 for span in line['spans']:
                     font_information = FontInformation(span['font'], span['flags'], span['color'], span['size'])
                     fonts_used.add(font_information)
+                    debug_content.append( (font_information, span['text']) )
                     if font_information not in text_with_formatting: text_with_formatting[font_information] = []
                     text_with_formatting[font_information].append(span['text'])
     pdf_document.close()
-    return PdfContent('\n'.join(full_text), text_with_formatting, fonts_used, images)
+    return PdfContent('\n'.join(full_text), text_with_formatting, fonts_used, images), debug_content
