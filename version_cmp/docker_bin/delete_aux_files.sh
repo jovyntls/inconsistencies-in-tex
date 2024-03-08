599d3298ee6dc9b9b35249aacada9459d7387717
Jovyn Tan <jovyn.tls@gmail.com>
2024-03-08 13:31:14 +0800

    more docker

---
+++ version_cmp/docker_bin/delete_aux_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+find . -name "*.aux" -type f -delete
+find . -name "*.lof" -type f -delete
+find . -name "*.gz" -type f -delete
+find . -name "*.toc" -type f -delete
+find . -name "*.bak" -type f -delete
+find . -name "*.synctex" -type f -delete
+find . -name "*.synctex.gz" -type f -delete
+find . -name "*.tex.latexmain" -type f -delete
+find . -name "*.run.xml" -type f -delete
+find . -name "*.nls" -type f -delete
+find . -name "*.nlo" -type f -delete
+find . -name "*.nlg" -type f -delete
+find . -name "*.brf" -type f -delete
+find . -name "*.nmo" -type f -delete
+find . -name "*.tdo" -type f -delete
+find . -name "*.snm" -type f -delete
+find . -name "*.tdo" -type f -delete
+find . -name "*.nav" -type f -delete
+find . -name "*.spl" -type f -delete
+find . -name "*.cb2" -type f -delete
+find . -name "*.cb" -type f -delete
+find . -name "*.fls" -type f -delete
+find . -name "*.fdb_latexmk" -type f -delete
+find . -name "*.blg" -type f -delete
+find . -name "*.bcf" -type f -delete
